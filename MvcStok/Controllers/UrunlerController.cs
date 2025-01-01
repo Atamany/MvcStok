948cd2b7f7cee82aa260ae82bceac5a1e7e7db2b
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-01-01 18:50:02 +0300

    UrunEkle

--- MvcStok/Controllers/UrunlerController.cs
+++ MvcStok/Controllers/UrunlerController.cs
@@ -7,40 +7,51 @@ using System.Web.Mvc;
 
 namespace MvcStok.Controllers
 {
-	public class UrunlerController : Controller
-	{
-		MvcDbStokEntities db = new MvcDbStokEntities();
-		public ActionResult UserIndex()
-		{
-			int ktg = Convert.ToInt32(Request.QueryString["UrunKategori"]);
-			var degerler = db.Tbl_Urunler.Where(x => x.UrunStok > 0 && x.Durum == true && x.UrunKategori == ktg).ToList();
-			return View(degerler);
-		}
-		public ActionResult Index()
-		{
-			int ktg = Convert.ToInt32(Request.QueryString["UrunKategori"]);
-			var degerler = db.Tbl_Urunler.Where(x => x.Durum == true && x.UrunKategori == ktg).ToList();
-			return View(degerler);
-		}
+    public class UrunlerController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult UserIndex()
+        {
+            int ktg = Convert.ToInt32(Request.QueryString["UrunKategori"]);
+            var degerler = db.Tbl_Urunler.Where(x => x.UrunStok > 0 && x.Durum == true && x.UrunKategori == ktg).ToList();
+            return View(degerler);
+        }
+        public ActionResult Index()
+        {
+            int ktg = Convert.ToInt32(Request.QueryString["UrunKategori"]);
+            var degerler = db.Tbl_Urunler.Where(x => x.Durum == true && x.UrunKategori == ktg).ToList();
+            return View(degerler);
+        }
 
-		[HttpGet]
-		public ActionResult YeniUrun()
-		{
-			return View();
-		}
-		[HttpPost]
-		public ActionResult YeniUrun(Tbl_Urunler urn)
-		{
-			db.Tbl_Urunler.Add(urn);
-			db.SaveChanges();
-			return View();
-		}
-		public ActionResult UrunSil(int id)
-		{
-			var urun = db.Tbl_Urunler.Find(id);
-			urun.Durum = false;
-			db.SaveChanges();
-			return RedirectToAction("Index", "Urunler", new { UrunKategori = urun.UrunKategori });
-		}
-	}
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            List<SelectListItem> ktg = db.Tbl_Kategori.
+                Select(x => new SelectListItem
+                {
+                    Text = x.KategoriAd,
+                    Value = x.KategoriId.ToString()
+                }).ToList();
+            ViewBag.drop = ktg;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniUrun(Tbl_Urunler p)
+        {
+            p.Durum = true;
+            var ktgr = db.Tbl_Kategori.Where(x => x.KategoriId == p.Tbl_Kategori.KategoriId).FirstOrDefault();
+            p.Tbl_Kategori = ktgr;
+            db.Tbl_Urunler.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Urunler", new { UrunKategori = p.UrunKategori });
+        }
+        public ActionResult UrunSil(int id)
+        {
+            var urun = db.Tbl_Urunler.Find(id);
+            urun.Durum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index", "Urunler", new { UrunKategori = urun.UrunKategori });
+        }
+
+    }
 }
